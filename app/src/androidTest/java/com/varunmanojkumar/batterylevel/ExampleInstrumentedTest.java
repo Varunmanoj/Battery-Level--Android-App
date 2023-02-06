24ee931c9a710dd25038161dcfe1eed557122090
Varunmanoj <varunmanoj2002@gmail.com>
2023-02-06 12:18:35 +0530

    Added Expreso Tests for Button Text

--- app/src/androidTest/java/com/varunmanojkumar/batterylevel/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/varunmanojkumar/batterylevel/ExampleInstrumentedTest.java
@@ -1,12 +1,14 @@
 package com.varunmanojkumar.batterylevel;
 
-import static org.junit.Assert.assertEquals;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
-import android.content.Context;
-
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.platform.app.InstrumentationRegistry;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -17,10 +19,16 @@ import org.junit.runner.RunWith;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenarioRule = new ActivityScenarioRule<>(MainActivity.class);
+
+    // test to check if the preferred language
+    // of user is displayed under the chosen language or not
+
     @Test
     public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.batterylevel", appContext.getPackageName());
+        onView(withId(R.id.speak)).check(matches(withText("Say It!")));
+
     }
 }
